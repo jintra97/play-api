c01435fcda6407e9b69813dffdd97eedcdc12082
Jintra <services@jintra.nl>
2025-01-22 07:50:30 +0100

    CORS headers toegevoegd (api requests van buiten toestaan)

--- api/now-playing.js
+++ api/now-playing.js
@@ -1,33 +1,11 @@
-async function getAccessToken() {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
-        ).toString("base64")}`,
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: new URLSearchParams({
-        grant_type: "refresh_token",
-        refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
-      })
-    });
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // of specifieker: "http://localhost:3000"
+    res.setHeader("Access-Control-Allow-Methods", "GET");
   
-    if (!response.ok) {
-      throw new Error("Fout bij ophalen access token");
-    }
-  
-    const data = await response.json();
-    return data.access_token;
-  }
-  
-  export default async function handler(req, res) {
     try {
-      const accessToken = await getAccessToken();
-  
       const response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
         headers: {
-          Authorization: `Bearer ${accessToken}`
+          Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
         }
       });
   
